0c5a23825fbf8ccafbe577e2e3912d15cab569cc
alexahs <alexander.harold.sexton@gmail.com>
2019-11-11 12:02:01 +0100

    small change

--- project2/src/tests.py
+++ project2/src/tests.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from sklearn import datasets, linear_model
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 from pylearn.logisticregression import *
 
@@ -21,29 +21,38 @@ class Test():
 
         X_train, X_test, y_train, y_test = train_test_split(data, target, test_size = 0.2)
 
-        scaler = StandardScaler()
+        scaler = MinMaxScaler(feature_range=(-1,1))
         scaler.fit(X_train)
         X_train_scaled = scaler.transform(X_train)
         X_test_scaled = scaler.transform(X_test)
 
-
-
         clf = linear_model.LogisticRegressionCV()
-        clf.fit(X_train_scaled, y_train)
-        pred_skl = clf.predict(X_test_scaled)
-
         model = SGDClassification()
-        model.fit(X_train_scaled, y_train)
-        pred_model = model.predict(X_test_scaled)
-
-        accuracy_test_skl = accuracy_score(pred_skl, y_test)
-        accuracy_test_model = accuracy_score(pred_model, y_test)
-        accuracy_model_skl = accuracy_score(pred_skl, pred_model)
-
-        print('scikit accuracy:', accuracy_test_skl)
-        print('model accuracy:', accuracy_test_model)
 
 
+
+        acc_model = np.zeros(10)
+        acc_skl = np.zeros(10)
+
+
+        for i in range(len(acc_model)):
+            clf.fit(X_train_scaled, y_train)
+            pred_skl = clf.predict(X_test_scaled)
+
+            model.fit(X_train_scaled, y_train)
+            pred_model = model.predict(X_test_scaled)
+
+            acc_skl[i] = accuracy_score(pred_skl, y_test)
+            acc_model[i] = accuracy_score(pred_model, y_test)
+
+        print('scikit accuracy:', np.mean(acc_skl))
+        print('pylearn accuracy:', np.mean(acc_model))
+        print('diff:', abs(np.mean(acc_skl) - np.mean(acc_model)))
+
+
+    def testNeuralNetwork(self):
+
+        pass
 
 
 
